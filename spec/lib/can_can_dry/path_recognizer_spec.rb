d92464d5ac69709fb8fb6e6b302c73da3eeef1ff
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-03-19 22:33:08 -0300

    RSpec.describe(CanCanDry::PathRecognizer): convertido de CanCanDry::PathRecognizerTest.

---
+++ spec/lib/can_can_dry/path_recognizer_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'can_can_dry/path_recognizer'
+
+RSpec.describe ::CanCanDry::PathRecognizer do
+  describe '#remove_root_path' do
+    [%w[/path/to/action /prefix/ /prefix/path/to/action],
+     %w[/path/to/action /prefix /prefix/path/to/action],
+     %w[/path / /path],
+     ['/path', '', '/path'],
+     %w[/unknown/path /abc /unknown/path]].each do |s|
+      it do
+        expect(described_class.send(:remove_root_path, s[1], s[2])).to eq(s[0])
+      end
+    end
+  end
+end
