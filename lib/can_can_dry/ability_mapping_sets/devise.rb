1fc0437c06ca23b297b19d014b56120076af7068
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-03-24 16:56:33 -0300

    CanCanDry::Ability::MappingSets::Devise.map_devise: maps all Devise controllers.

--- lib/can_can_dry/ability_mapping_sets/devise.rb
+++ lib/can_can_dry/ability_mapping_sets/devise.rb
@@ -4,8 +4,12 @@ module CanCanDry
   module AbilityMappingSets
     module Devise
       def map_devise
+        map_resources 'Devise::Confirmation'
+        map_resources 'Devise::OmniauthCallback'
+        map_resources 'Devise::Registration'
         map_resources 'Devise::Session'
         map_resources 'Devise::Password'
+        map_resources 'Devise::Unlock'
       end
     end
   end
