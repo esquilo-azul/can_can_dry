a4fb39f46d3ea9ca2b75d90014c69e3ec4eb391b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-03-25 18:26:06 -0300

    CanCanDry::Ability::MappingSets::DeviseInvitable.map_devise_invitable: maps "Devise::Invitations".

--- lib/can_can_dry/ability_mapping_sets/devise_invitable.rb
+++ lib/can_can_dry/ability_mapping_sets/devise_invitable.rb
@@ -4,6 +4,7 @@ module CanCanDry
   module AbilityMappingSets
     module DeviseInvitable
       def map_devise_invitable
+        map_resources 'Devise::Invitations'
         map_resources 'DeviseInvitable::Registration'
       end
     end
