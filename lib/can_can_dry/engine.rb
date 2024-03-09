1a365f190d03292ab0c7c62a2e4abf0ff68a27d4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-09 20:44:16 -0300

    CanCanDry::Engine: include EacRailsUtils::EngineHelper.

--- lib/can_can_dry/engine.rb
+++ lib/can_can_dry/engine.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require 'cancancan'
+require 'eac_rails_utils/engine'
 
 module CanCanDry
-  class Engine < ::Rails::Engine; end
+  class Engine < ::Rails::Engine
+    include ::EacRailsUtils::EngineHelper
+  end
 end
