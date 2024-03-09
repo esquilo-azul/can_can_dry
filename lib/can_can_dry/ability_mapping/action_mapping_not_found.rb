520ad6827966dcf7820fec7b131b5c754d03762e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-09 13:11:32 -0300

    .rubocop.yml#Layout/LineEndStringConcatenationIndentation: remove.

--- lib/can_can_dry/ability_mapping/action_mapping_not_found.rb
+++ lib/can_can_dry/ability_mapping/action_mapping_not_found.rb
@@ -5,7 +5,7 @@ module CanCanDry
     class ActionMappingNotFound < RuntimeError
       def initialize(controller, action)
         super('Nenhum mapeamento de controle de acesso encontrado ' \
-            "para a action \"#{controller}##{action}\"")
+              "para a action \"#{controller}##{action}\"")
       end
     end
   end
